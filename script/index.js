09a7ced97c9dc6b687189ccdd67da0478030d3dc
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2022-11-10 12:20:03 -0300

    paginapessoal

---
+++ script/index.js
@@ -0,0 +1,17 @@
+var sobremin = document.getElementById("divSobre");
+var video = document.getElementById("divVideo");
+var musica = document.getElementById("divmusica");
+var botaoMusica = document.getElementById("musica");
+
+function sobreMin(){
+    sobremin.style.display = "flex"
+}
+
+function videoYoutube() {
+    video.style.display = "flex";
+}
+
+function musicas() {
+    musica.style.display = "flex"
+    botaoMusica.style.display = "none";
+}
